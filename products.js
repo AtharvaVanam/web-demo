fdd48d14757ce4e888e712e12e2008f3e0a384a7
Atharva <admin@mitnotes.com>
2026-04-18 00:51:02 +0530

    css change

--- products.js
+++ products.js
@@ -3,12 +3,12 @@ const products = [
     id: 1,
     name: "Oversized T-Shirt",
     price: 799,
-    https://picsum.photos/200
+    image: "https://picsum.photos/200/200"
   },
   {
     id: 2,
     name: "Hoodie",
     price: 1499,
-    https://picsum.photos/200
+    image: "https://picsum.photos/200/200"
   }
 ];
