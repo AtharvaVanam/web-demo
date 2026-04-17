b4f09c14c34fa70d92fd42b2f1cee1fc7e31bc6e
Atharva <admin@mitnotes.com>
2026-04-18 01:12:15 +0530

    updated images

--- products.js
+++ products.js
@@ -3,12 +3,12 @@ const products = [
     id: 1,
     name: "Oversized T-Shirt",
     price: 799,
-    image: "https://picsum.photos/200/200"
+    image: "https://images.unsplash.com/photo-1542291026-7eec264c27ff"
   },
   {
     id: 2,
     name: "Hoodie",
     price: 1499,
-    image: "https://picsum.photos/200/200"
+    image: "https://images.unsplash.com/photo-1542291026-7eec264c27ff"
   }
 ];
